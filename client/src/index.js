b023bd9a0f13a185948ac8f306f9b39033d584bb
Alex Choi <alexchoiweb@gmail.com>
2018-12-22 07:40:12 +0000

    try iphone pwa install popup

--- client/src/index.js
+++ client/src/index.js
@@ -13,11 +13,15 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
-// if('serviceWorker' in navigator) {
-//   try {
-//     navigator.serviceWorker.register('sw.js');
-//     console.log(`SW registered`)
-//   } catch (error) {
-//     console.log(`SW registration failed`);
-//   }
-// }
+// Detects if device is on iOS 
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}
